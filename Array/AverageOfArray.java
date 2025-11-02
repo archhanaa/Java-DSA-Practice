76a9a70033f45dc8f2cd28743c7aa2edeaa6ca47
archhanaa <archanaaa0623@gmail.com>
2025-11-02 22:52:42 +0530

    Added program to find average of array

---
+++ Array/AverageOfArray.java
@@ -0,0 +1,12 @@
+class AverageOfArray{
+  public static void main(String[]args){
+    int[] array = {10,20,30,40,50};
+    int sum = 0;
+    for(int i=0;i<array.length;i++){
+      sum += array[i];
+    }
+    int average = sum/array.length;
+    System.out.println(average);
+  }
+}  
+      
